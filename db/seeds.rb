192400a1f525a9a884a5b9e2dfc32261b70c8619
Brad.Harper <Brad.Harper@consensuscorp.com>
2019-01-05 11:20:24 -0700

    Seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do
+  department = Department.create(title: Faker::Commerce.department)
+  2.times do
+    product = Product.create(
+        department_id: department.id,
+        name: Faker::Commerce.product_name,
+        description: Faker::Lorem.sentence,
+        price: Faker::Commerce.price.to_f,
+        stock: 1 + rand(100)
+    )
+    5.times do
+      Review.create(
+       product_id: product.id,
+        subject: Faker::Commerce.material,
+        body: Faker::Lorem.sentence,
+        stars: 1 + rand(5),
+        date: Time.at(rand * Time.now.to_i)
+      )
+    end
+  end
+end
