95ad46ee21ba50a5661ecb3525888540180f7590
Nikki Johnson <nikkijohnson@Nikkis-MacBook-Air.local>
2019-01-05 12:07:10 -0700

    reviews

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -1,10 +1,77 @@
 import React from 'react';
+import { Form, Button, Select } from 'semantic-ui-react';
 
 class ReviewForm extends React.Component {
+    defaultValues = { subject: '', body: '', stars: '', date: '' };
+    state = { subject: '', body: '', stars: '', date: '' };
+
+    componentDidMount() {
+        if (this.props.id) {
+            this.setState({...this.props})
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const review = {...this.state}
+        this.props.submit(review)
+        this.setState({...this.defaultValues})
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value, })
+    }
+
     render () {
+        const starOptions = [
+            {key: '1', text: '1', value: '1'},
+            {key: '2', text: '2', value: '2'},
+            {key: '3', text: '3', value: '3'},
+            {key: '4', text: '4', value: '4'}
+        ]
         return (
             <div>
-
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Field>
+                    <label>Subject</label>
+                    <input 
+                    name="subject"
+                    placeholder="Subject"
+                    value={this.state.subject}
+                    onChange={this.handleChange}
+                        />
+                    </Form.Field>
+                    <Form.TextArea>
+                    <label>Body</label>
+                    <input 
+                    type=""
+                    name="body"
+                    placeholder="Your Review Here"
+                    value={this.state.body}
+                    onChange={this.handleChange} 
+                    />
+                    </Form.TextArea>
+                    <Button type='submit'>Submit</Button>
+                    <Form.Field
+                        control={Select}
+                        option={starOptions}
+                        label={{ children: 'Stars', htmlFor: 'form-select-control-gender' }}
+                        placeholder= 'Stars'
+                        name="stars"
+                        value={this.state.stars}
+                        onChange={this.handleChange}
+                    >
+                    </Form.Field>
+                    <Form.Field>
+                    <label>Date</label>
+                    <input 
+                    name="date"
+                    placeholder="Date"
+                    value={this.state.date}
+                    onChange={this.handleChange}
+                        />
+                    </Form.Field>
+                </Form>
             </div>
         )
     }
--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import ReviewForm from './ReviewForm';
+import axios from 'axios';
+import {Header, Segment, Button, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom'
 
 class Reviews extends React.Component {
+    state = { reviews: [], showForm: false, edit: false }
+
+    componentDidMount() {
+        axios.get(`/api/departments/${this.props.match.params.department_id}/products/${this.props.match.params.product_id}`)
+        .then(res => {
+            this.setState({ reviews: res.data })
+        })
+    }
+
+    toggleForm = () => {
+        this.setState( state => {
+            return { showForm: !state.showForm }
+        } )
+    }
+
+    edit = () => {
+        return <ReviewForm {...this.state.review} submit={this.submit} />
+    }
+
+    form = () => {
+        return <ReviewForm submit={this.submit} />
+    }
+
+    submit = (review) => {
+        axios.post(`/api/departments/${this.props.match.params.department_id}/products/${this.props.match.params.product_id}/reviews/`, {review})
+        .then( res => {
+            this.setState({reviews: [res.data, ...this.state.reviews], showForm: false})
+        })
+    }
+
+    renderReviews = () => {
+        return this.state.reviews.map( p => {
+            return (
+                <div {...this.props} >
+                    <Segment key={p.id} style={{textAlign: 'left'}}>
+                        <Header as="h2">{p.subject}</Header>
+                        <Header as="h3">Stars: {p.stars}</Header>
+                        <p>{p.body}</p>
+                        <p style={{color: 'grey'}}>{p.date}</p>
+                    <Button>
+                        <Link to={`/departments/${this.props.match.params.department_id}/products/${this.props.match.params.product_id}/reviews/${this.props.match.params.id}`}>
+                        Edit
+                        </Link>
+                    </Button>
+                    <Button
+                        icon 
+                        color="red"
+                        size="small"
+                        onClick={() => this.deleteReview(p.id)}
+                    >
+                    <Icon name="trash" />
+                    </Button>
+                    </Segment>
+                    </div>
+            )
+        })
+    }
+
+    deletePost = (id) => {
+        axios.delete(`/api/departments/${this.props.match.params.department_id}/products/${this.props.match.params.product_id}/reviews/${this.props.match.params.id}`)
+        .then ( res => {
+            const { reviews } = this.state
+            this.setState({ reviews: reviews.filter( t => t.id )})
+        } )
+    }
+
+
     render () {
+
         return (
             <div>
 
