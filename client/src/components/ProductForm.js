96fd287231446aafd69abf9e90acbd60c3dab18d
Adriana Vars <asvutah@gmail.com>
2019-01-05 12:16:24 -0700

    products and form

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import { Form } from 'semantic-ui-react';
 
-class ProductForm extends React.Component {
-    render () {
-        return (
-            <div>
+class ProductForm extends Component {
+  defaultValues = { name: '', description: '', price: '', stock: ''}
+  state = {...this.defaultValues}
 
-            </div>
-        )
-    }
-};
+  handleChange = (e) => {
+    const {name, value} = e.target
+    this.setState({ [name]: value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault(); 
+    const product = { ...this.state }
+    this.props.submit(product)
+    this.setState({ ...this.defaultValues })
+  }
+
+  render (){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="name"
+          placeholder="Name"
+          value={this.state.value}
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          name="description"
+          placeholder="Description"
+          value={this.state.value}
+          onChange={this.handleChange}
+          required
+        />
+           <input
+          name="price"
+          placeholder="Price"
+          value={this.state.value}
+          onChange={this.handleChange}
+          required
+        />
+           <input
+          name="Stock"
+          placeholder="Stock"
+          value={this.state.value}
+          onChange={this.handleChange}
+          required
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
 
 export default ProductForm;
--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,13 +1,64 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import ProductForm from './ProductForm';
 
 class Products extends React.Component {
+    state = { products: [], showForm: true }
+  
+    componentDidMount() {
+        axios.get(`/api/departments/${this.props.match.params.department_id}/products`)
+        .then( res => {
+          this.setState({ products: res.data });
+        })
+        .catch( err => {
+          console.log(err);
+        })
+    }
+
+    toggleForm = () => {
+        this.setState(state => {
+          return { showForm: !state.showForm }
+        })
+      }
+    
+      form = () => {
+        return <ProductForm submit={this.submit} />
+      }
+
+      listProducts = () => {
+        return this.state.products.map(p => {
+          return (
+            <div>
+              <Link key={p.id} to={`/departments/${this.props.match.params.department_id}/products`}> {p.name} </Link>
+            </div>
+           )
+        })
+      }
+
+      submit = (product) => { 
+        axios.post(`/api/departments/${this.props.match.params.department_id}/products`, {product})
+          .then( res => {
+            const { products } = this.state
+            this.setState({ products: [...products, res.data]})
+          })
+          .catch( err => {
+            console.log(err)
+          })
+      }
+
     render () {
+        const { showForm } = this.state
         return (
             <div>
-
+                <button onClick={this.toggleForm}>{ showForm ? 'Hide' : 'Show'}</button>
+                {showForm ? this.form() : this.listProducts()}
             </div>
         )
     }
 };
 
+
 export default Products;
+
+
