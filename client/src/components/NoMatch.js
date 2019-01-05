44c71310947e52c891c8aafa83bb38072c001480
Brad.Harper <Brad.Harper@consensuscorp.com>
2019-01-05 14:39:44 -0700

    updated no match

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -3,7 +3,7 @@ import { Header, Segment, Image, Divider } from 'semantic-ui-react';
 
 const NoMatch = () => (
     <Segment textAlign='center'>
-      <Header as='h1' textAlign='center'>404 - Not Found</Header>
+      <Header as='h1' textAlign='center'>You've been Rickrolled!</Header>
       <Image centered src='https://media.giphy.com/media/kFgzrTt798d2w/giphy.gif' size='large' />
       <Divider/>
       <Image centered src='https://media.giphy.com/media/lW9XPLjNXyDDO/giphy.gif' size='large' />
